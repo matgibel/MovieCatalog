689b3e8fb4752fbc6f7cb3bf51d8cd8116836454
Mathew Gibel <matgibel@gmail.com>
2021-02-14 19:02:36 -0500

    Added Entity Tools and SQL Server, Edit in startup and appsettings.json

--- TestApplication/Models/Video.cs
+++ TestApplication/Models/Video.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,7 @@ namespace TestApplication.Models
 {
     public class Video
     {
+        [Key]
         public int VideoId { get; set; }
         public String Title { get; set; }
         public String Director { get; set; }
@@ -17,6 +20,7 @@ namespace TestApplication.Models
 
         public decimal Price { get; set; }
         public bool InStock { get; set; }
+        [ForeignKey("Category")]
         public int CategoryId { get; set; }
         public Category Category { get; set; }
 
--- TestApplication/Startup.cs
+++ TestApplication/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TestApplication.Models;
@@ -13,10 +15,19 @@ namespace TestApplication
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"))
+            ); 
             services.AddControllersWithViews();
             services.AddScoped<IVideoRepository, MockVideoRepository>();
             services.AddScoped<ICategoryRepository, MockCategoryRepository>();
